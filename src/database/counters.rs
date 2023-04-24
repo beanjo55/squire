20a5fa89551742b93e096023ef44641c7b0b87dc
beanjo55 <abbyschweitzer22@gmail.com>
2023-04-24 18:37:28 -0400

    move counters handlers to counters.rs

--- src/database/counters.rs
+++ src/database/counters.rs
@@ -41,3 +41,28 @@ pub struct NewCounterEntry {
     pub source_guild_id: i64,
     pub user_id: i64,
 }
+pub fn get_counter_by_name(
+    conn: &mut PgConnection,
+    counter_name: &str,
+    guild_id: &u64,
+) -> Result<Option<Counter>, diesel::result::Error> {
+    use crate::database::schema::counters::dsl::*;
+    counters
+        .filter(name.eq(counter_name))
+        .filter(guild_ids.contains(vec![*guild_id as i64]))
+        .limit(1)
+        .first::<Counter>(conn)
+        .optional()
+}
+
+pub fn get_counter_by_id(
+    conn: &mut PgConnection,
+    counter_id: &str,
+) -> Result<Option<Counter>, diesel::result::Error> {
+    use crate::database::schema::counters::dsl::*;
+    counters
+        .find(Uuid::parse_str(counter_id).unwrap())
+        .limit(1)
+        .first::<Counter>(conn)
+        .optional()
+}
--- src/database/mod.rs
+++ src/database/mod.rs
@@ -2,34 +2,3 @@ pub mod counters;
 pub mod highlights;
 pub mod home_assistant;
 pub mod schema;
-
-use diesel::prelude::*;
-use uuid::Uuid;
-
-use self::counters::Counter;
-
-pub fn get_counter_by_name(
-    conn: &mut PgConnection,
-    counter_name: &str,
-    guild_id: &u64,
-) -> Result<Option<Counter>, diesel::result::Error> {
-    use crate::database::schema::counters::dsl::*;
-    counters
-        .filter(name.eq(counter_name))
-        .filter(guild_ids.contains(vec![*guild_id as i64]))
-        .limit(1)
-        .first::<Counter>(conn)
-        .optional()
-}
-
-pub fn get_counter_by_id(
-    conn: &mut PgConnection,
-    counter_id: &str,
-) -> Result<Option<Counter>, diesel::result::Error> {
-    use crate::database::schema::counters::dsl::*;
-    counters
-        .find(Uuid::parse_str(counter_id).unwrap())
-        .limit(1)
-        .first::<Counter>(conn)
-        .optional()
-}
